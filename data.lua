493b9154101514011d04c853e7e0213550f7ef89
JNjiaonang <101388491+JNjiaonang@users.noreply.github.com>
2024-11-19 13:06:28 +0800

    Update data.lua

--- data.lua
+++ data.lua
@@ -8,7 +8,7 @@ data:extend({
         localised_name = {"shortcut.toggle-machine-exp-gui"},  -- Ensure this key exists in your locale
         associated_control_input = "toggle-machine-exp-key",  -- Connects to the keybinding
         icon = "__base__/graphics/icons/shortcut-toolbar/mip/alt-mode-x32.png",  -- Use a base game icon path
-        icon_size = 32,  -- Size of the icon
+        icon_size = 24,  -- Size of the icon
         small_icon = "__base__/graphics/icons/shortcut-toolbar/mip/alt-mode-x24.png",  -- Small icon path
         small_icon_size = 24,  -- Size of the small icon
         toggleable = true  -- Makes the button act as a toggle
